add578e2eb278f92690917d406ed9e05992474f7
Yari-i <tipo47181@gmail.com>
2024-08-23 22:59:42 +0300

    course work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee employee1 = new Employee("Иван Иванович Иванов", 1, 20000);
+        Employee employee2 = new Employee("Иван Иванович Иванов", 1, 20000);
+        System.out.println(employee1);
+        System.out.println(employee2);
     }
 }
